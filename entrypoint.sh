9deb918251ce4366d231da98bb19abefd9c7b841
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2019-10-01 00:04:33 +0200

    add an example

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,37 @@
-#!/bin/sh -l
+#!/usr/bin bash
+set -e
+# Arguments Parser
+BUNDLE="app.flatpak"
+RUNTIME_REPO="https://flathub.org/repo/flathub.flatpakrepo"
 
-echo "I'm ready"
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case $key in
+    --manifest-path)
+      MANIFEST_PATH="$2"
+      shift
+      shift
+    ;;
+    --bundle)
+      BUNDLE="$2"
+      shift
+      shift
+    ;;
+    --runtime-repo)
+      RUNTIME_REPO="$2"
+      shift
+      shift
+    ;;
+    --app-id)
+      export APP_ID="$2"
+      shift
+      shift
+    ;;
+  esac
+done
+
+
+flatpak-builder flatpak_app ${MANIFEST_PATH}
+flatpak-builder --repo=repo --force-clean flatpak_app ${MANIFEST_PATH}
+flatpak build-bundle repo ${BUNDLE} --runtime-repo=${RUNTIME_REPO} ${APP_ID} master
