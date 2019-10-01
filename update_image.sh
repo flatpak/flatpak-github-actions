739ef4e6921fe5d692ffe4081fbe32b293bd45c0
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2019-10-01 15:11:09 +0200

    Update image name

--- update_image.sh
+++ update_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-NAME=docker.io/bilelmoussaoui/flatpak-builder-github-actions
+NAME=docker.io/bilelmoussaoui/flatpak-github-actions
 
 sudo docker build . -t "$NAME"
 sudo docker push "$NAME"
