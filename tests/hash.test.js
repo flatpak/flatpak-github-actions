1b1c9053e355d1d8bcc1aa80aba50c4fe17444f7
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-04-01 15:55:00 +0200

    CI: bump to GNOME 40 and use the new docker images

--- tests/hash.test.js
+++ tests/hash.test.js
@@ -3,8 +3,8 @@ const { computeHash} = require('../index')
 
 test('The manifest hash should be computed properly', async () => {
     const hash = await computeHash('./tests/manifest-1.yaml')
-    expect(hash).toBe('df49da0581c3914b06ef48122b3a5003d91350acfba6972a9bf26ac9fb3f7ae6')
+    expect(hash).toBe('29ed9085544904a2bca91a82c487b5d33961650f488743473f801ccde9279ae7')
 
     const hash2 = await computeHash('./tests/manifest-3.json')
-    expect(hash2).toBe('21effbaab820d8f4fe2d415c1774bc0a80fe6b45e6ada5720515c9a2f0eeedd0')
+    expect(hash2).toBe('f75c438a29d4f04b7b53801a653e485fbdb773a284d9f53ae29087e927b2bdfe')
 })
