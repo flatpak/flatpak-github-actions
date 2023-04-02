b0e5499295687db87d532d318abec3b8620de288
Bilal Elmoussaoui <belmouss@redhat.com>
2023-04-02 02:35:24 +0200

    Stop using deprecated runtime

--- flatpak-builder/tests/hash.test.js
+++ flatpak-builder/tests/hash.test.js
@@ -2,8 +2,8 @@ const { computeHash } = require('../index')
 
 test('The manifest hash should be computed properly', async () => {
   const hash = await computeHash('./tests/manifest-1.yaml')
-  expect(hash).toBe('29ed9085544904a2bca91a82c487b5d33961650f488743473f801ccde9279ae7')
+  expect(hash).toBe('8c43f78c9f33c1f379521211c9b9e91fe051cef3f638e4427026beb27261a587')
 
   const hash2 = await computeHash('./tests/manifest-3.json')
-  expect(hash2).toBe('f75c438a29d4f04b7b53801a653e485fbdb773a284d9f53ae29087e927b2bdfe')
+  expect(hash2).toBe('fb531a4e4d3227ff9cdb37b8d1b9830878aa63542a066c2bd157dd94c7dda1ba')
 })
